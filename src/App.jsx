5fdc35b8f3c2ad367f56c1445fed6c98bfef8feb
Abhishek-Ahuja <abhishekahuja576@gmail.com>
2023-03-02 02:21:14 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './component/SignUP/SignUp'
 import Login from './component/Login/Login'
 import OtpPage from './component/OtpSection/OtpPage'
@@ -8,7 +9,13 @@ function App() {
   
   return (
     <div className="App">
-        <OtpPage />
+        <Router>
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/signUp' element={<SignUp />} />
+            <Route path='/otp' element={<OtpPage />} />
+          </Routes>
+        </Router>
     </div>
   )
 }
